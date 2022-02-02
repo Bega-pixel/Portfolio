a7c7db08d1aa597efe1aab3e9f0b1a66bce90288
Bega-pixel <bega2000@protonmail.com>
2022-02-02 17:30:17 +0100

    React

---
+++ app/portfolio/src/components/cover/Cover.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Cover.css";
+
+export const Cover = () => {
+  return<div className="cover-container">
+          <video className='video' src='https://res.cloudinary.com/dazypdtba/video/upload/v1643363846/portfolio/pexels-tima-miroshnichenko-5377697_bz5upg.mp4' autoPlay loop muted/> 
+        </div>
+};
+export default Cover;

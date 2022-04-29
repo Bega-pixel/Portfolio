18e38e11764aa7ccce4aa912a6bf825ec314d162
Bega-pixel <bega2000@protonmail.com>
2022-04-29 11:31:45 +0200

    fix bugs

--- app/portfolio/src/components/about/About.js
+++ app/portfolio/src/components/about/About.js
@@ -7,15 +7,13 @@ const About = () => {
         <div className="about-desc">
             <h3>Hello</h3>
             <p> Passionate coder. I work hard, care about writing clean code and I
-                genuinely like to Learn. I ́m currently looking for the right opportunity to help
+                genuinely like to Learn. I'm currently looking for the right opportunity to help
                 me in a environment that will help me increase my progress in to a Full-stack
                 role.</p>
 
             <h3>Skills</h3> 
-            <p>HTML, CSS, SASS, Javascript, JQuery, React, Express, Node.js, MongoDB
-               Creative, Teamwork, Ability to interact professionally with clients and 
-               co-workers, both verbally and in writing, stress tolerance
-               and ability to meet tight deadlines.</p>   
+            <p>HTML, CSS, SASS, Javascript, JQuery, React, Express, Node.js, MongoDB,
+               Creative, Teamwork.</p>   
         </div>
         <div>
             <img className="about-img" src="https://res.cloudinary.com/dazypdtba/image/upload/v1646931411/portfolio/desk_eilce6.jpg" alt="about" />
